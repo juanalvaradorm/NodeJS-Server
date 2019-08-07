e3aa1fd780c084eabd319cb446e67680b2fc62cd
Juan Manuel Alvarado <juan.alvarado@gigigo.com>
2019-08-07 14:12:39 -0500

    Modify Variable de entorno Heroku

--- Server/Config/Config.js
+++ Server/Config/Config.js
@@ -19,7 +19,7 @@ let UrlDB;
 if (process.env.NODE_ENV === 'dev') {
     UrlDB = 'mongodb://localhost:27017/Cafe';
 } else {
-    UrlDB = 'mongodb+srv://UdemyJuanAR:rh8Uzr1Megezr77z@udemyjm-cudok.mongodb.net/Cafe';
+    UrlDB = process.env.CONNECT_URI;
 }
 
 //MongoDB: mongodb + srv: //UdemyJuanAR:<password>@udemyjm-cudok.mongodb.net/test
