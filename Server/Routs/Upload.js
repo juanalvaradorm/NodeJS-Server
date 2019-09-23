3bef838bc1cbce14797f2ab978f3a59b5367a30a
Juan Manuel Alvarado <juan.alvarado@gigigo.com>
2019-09-23 16:52:34 -0500

    Add file system test modify 2

--- Server/Routs/Upload.js
+++ Server/Routs/Upload.js
@@ -71,7 +71,7 @@ App.put('/Upload/:Type/:Id', function(req, res) {
             });
         }
 
-        if (Id === "1") {
+        if (Type === "1") {
             ImageUser(Id, res, FileName);
         } else {
             ImageItems(Id, res, FileName);
